1a9da5a6e480e623f135348f5158ff48e897b37b
Rogier Konings <rogierkonings@RogiersillPC469.home>
2019-07-18 20:40:06 +0200

    remove build

--- build/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require('express');
-var router = express.Router();
-router.get('/hello', function (req, res, next) { return res.send({ msg: 'Hello, Scaffold !!' }); });
-module.exports = router;
-//# sourceMappingURL=index.js.map
